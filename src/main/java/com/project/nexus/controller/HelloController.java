5f012149b7ab5a677e3d1aae1ad670fc8e4d2583
Juan <juanmanuelherreraaguayo@gmail.com>
2025-11-06 11:30:07 -0600

    Configuracion inicial del proyecto

---
+++ src/main/java/com/project/nexus/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.project.nexus.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Servidor Spring Boot funcionando correctamente 🚀";
+    }
+}
